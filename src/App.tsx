af612c05dd4ba06bb7ac9dd6a2f343a6730e2b6f
Buga <swd543@gmail.com>
2019-03-23 00:45:00 +0530

    Changed css and js

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,113 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Tic from "./components/Tic";
 
-class App extends Component {
+interface IState {
+  game: (boolean | null)[][];
+  xTurn: boolean;
+}
+interface IProps {}
+
+class App extends Component<IProps, IState> {
+  initState = () => ({
+    game: [[null, null, null], [null, null, null], [null, null, null]],
+    xTurn: true
+  });
+
+  state = this.initState();
+
+  resetState = () => {
+    this.setState(this.initState());
+  };
+
+  clickHandler = (x: number, y: number) => {
+    const { game } = this.state;
+
+    if (game[x][y] === null) {
+      this.setState(
+        prevState => {
+          prevState.game[x][y] = prevState.xTurn;
+          return { game: prevState.game, xTurn: !prevState.xTurn };
+        },
+        () => {
+          this.checkWin();
+        }
+      );
+    } else {
+      alert("Dude, that spot is already taken.");
+    }
+  };
+
+  checkWin = () => {
+    const { game } = this.state;
+
+    // Horizontal, vertical check
+    for (var i = 0; i < game.length; i++) {
+      var xCountHor = 0,
+        oCountHor = 0,
+        xCountVer = 0,
+        oCountVer = 0;
+      for (var j = 0; j < game[i].length; j++) {
+        game[i][j] === true && xCountHor++;
+        game[j][i] === true && xCountVer++;
+        game[i][j] === false && oCountHor++;
+        game[j][i] === false && oCountVer++;
+      }
+      if (xCountHor === game.length || xCountVer === game.length) {
+        alert("X wins!");
+      } else if (oCountHor === game.length || oCountVer === game.length) {
+        alert("O wins!");
+      }
+    }
+
+    // Diagonal check
+    {
+      var xCountLD = 0,
+        oCountLD = 0,
+        xCountRD = 0,
+        oCountRD = 0;
+      for (var i = 0; i < game.length; i++) {
+        game[i][i] === true && xCountLD++;
+        game[i][game.length - i - 1] === true && xCountRD++;
+        game[i][i] === false && oCountLD++;
+        game[i][game.length - i - 1] === false && oCountRD++;
+      }
+      if (xCountLD === game.length || xCountRD === game.length) {
+        alert("X wins!");
+      } else if (oCountLD === game.length || oCountRD === game.length) {
+        alert("O wins!");
+      }
+    }
+  };
+
   render() {
+    const { game, xTurn } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header className="App-header">Tic tac toe by Takti Nanu</header>
+        <div className="App-body">
+          <table>
+            <tbody>
+              {game.map((row, i) => (
+                <tr key={i}>
+                  {row.map((cell, j) => (
+                    <td key={j}>
+                      <Tic
+                        value={cell}
+                        clickHandler={this.clickHandler.bind(this, i, j)}
+                      />
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            {xTurn === true && "Hey X, its your turn!"}
+            {xTurn === false && "Hey O, its your turn!"}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <button onClick={this.resetState}>Reset</button>
+        </div>
       </div>
     );
   }
